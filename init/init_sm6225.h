de48866dd9f6d1771b43e7b0fd74ad8b3c20c9b8
root <root@localhost>
2024-03-11 00:02:11 +0000

    a23: initial common-tree

---
+++ init/init_sm6225.h
@@ -0,0 +1,33 @@
+#ifndef INIT_SM6225_H
+#define INIT_SM6225_H
+
+#include <string.h>
+
+enum device_variant {
+    VARIANT_A235F = ,
+    VARIANT_A235N,
+    VARIANT_A235M,
+    VARIANT_MAX
+};
+
+typedef struct {
+    std::string model;
+    std::string codename;
+} variant;
+
+static const variant europe_models_a23 = {
+    .model = "SM-A235F",
+    .codename = "a23"
+};
+
+static const variant asia_models_a23 = {
+    .model = "SM-A235M",
+    .codename = "a23"
+};
+
+static const variant *all_variants[VARIANT_MAX] = {
+    &europe_models_a23,
+    &asia_models_a23,
+};
+
+#endif // INIT_SM6225_H
